97d58a72f4ef400b0aef897eae72af6d9edf23f3
Meltem <you@example.com>
2022-09-07 20:05:14 +0300

    register added

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from multiprocessing import context
+from ssl import _create_default_https_context
 from django.shortcuts import render,redirect
+from django.db.models import Q
 from .models import Room,Topic
 from .forms import RoomForm
 """
@@ -11,10 +13,25 @@ rooms=[
     ]
 
 """ 
+def loginPage(request):
+    context={}
+    return render(request,"login_register.html",context)
+
+
+
+
 def home(request):
-  rooms = Room.objects.all()
+  q = request.GET.get("q") if request.GET.get("q")!= None else ""
+  rooms = Room.objects.filter(
+  Q(topic__name__icontains=q)|
+  Q(name__icontains=q)|
+  Q(description__icontains=q)
+  )
+
   topics=Topic.objects.all()
-  context ={"rooms":rooms,"topics":topics}
+
+  room_count=rooms.count()
+  context ={"rooms":rooms,"topics":topics,"room_count":room_count}
   return render(request , "home.html",context)
 
 
