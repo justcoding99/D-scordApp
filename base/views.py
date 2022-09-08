5bb2f92c7210b3fd5fb13d28797ba67c7a58a970
Meltem <you@example.com>
2022-09-08 18:21:50 +0300

    theme added

--- base/views.py
+++ base/views.py
@@ -103,7 +103,12 @@ def room(request,pk):
 def userProfile(request,pk):
     user= User.objects.get(id=pk)
     rooms = user.room_set.all()
-    context={"user":user,"rooms":rooms}
+    room_messages= user.message_set.all()
+    topics= Topic.objects.all()
+    context={"user":user,"rooms":rooms,
+             "room_messages":room_messages,
+              "topics":topics
+              }
     return render(request,'profile.html',context)
 
 
@@ -117,7 +122,9 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room= form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect("home")
 
     context={"form":form}
